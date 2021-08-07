63bd92e9550ae30a93c0d7feda096cbdc70e4eb2
Alex dark <alexdark2149@gmail.com>
2021-08-07 12:28:03 +0300

    created migration/controller/model

---
+++ database/migrations/2021_08_07_084901_create_transports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transports', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('type_of_travel');
+            $table->unsignedSmallInteger('cost');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transports');
+    }
+}
---
+++ database/migrations/2021_08_07_085042_create_travel_histories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel_histories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('card_id');
+            $table->unsignedSmallInteger('from_city_id');
+            $table->unsignedSmallInteger('to_city_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel_histories');
+    }
+}
